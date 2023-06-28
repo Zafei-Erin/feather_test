efca294f6ac6d8b3ae6059fa1984e9edc236e05a
Developer Experience Team <feather@terra.money>
2023-06-28 17:14:46 +0800

    Initialized with Feather

---
+++ cmd/feather-core/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"os"
+
+	"github.com/cosmos/cosmos-sdk/server"
+	svrcmd "github.com/cosmos/cosmos-sdk/server/cmd"
+
+	"github.com/Zafei-Erin/feather_test/app"
+	"github.com/Zafei-Erin/feather_test/cmd/feather-core/cmd"
+)
+
+func main() {
+	rootCmd, _ := cmd.NewRootCmd()
+	if err := svrcmd.Execute(rootCmd, "", app.DefaultNodeHome); err != nil {
+		switch e := err.(type) {
+		case server.ErrorCode:
+			os.Exit(e.Code)
+
+		default:
+			os.Exit(1)
+		}
+	}
+}
